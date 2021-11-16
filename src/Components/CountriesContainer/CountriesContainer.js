431b09adb4f05518ff5356ec815f3043c449aaa4
Dan <danielsalazar.cc@gmail.com>
2021-11-16 00:07:14 -0400

    fix: Change country card container to new data

--- src/Components/CountriesContainer/CountriesContainer.js
+++ src/Components/CountriesContainer/CountriesContainer.js
@@ -2,23 +2,22 @@ import React from 'react';
 import { Countries, Ancle } from './style';
 import Country from '../Country/Country';
 
-function CountriesContainer( { countries} ) {
-
-    return (
-        <Countries>
-                {
-                    countries.map(country => <Ancle key={country.numericCode} to={'/' + country.name}>
-                        <Country
-                            svg={country.flag}
-                            name={country.name}
-                            region={country.region} 
-                            capital={country.capital}
-                            population={country.population}
-                        />
-                    </Ancle>)
-                }
-        </Countries>
-    )
+function CountriesContainer({ countries }) {
+  return (
+      <Countries>
+        {countries.map((country, id) => (
+          <Ancle key={id} to={'/' + country.name.official}> 
+           <Country 
+             svg={country.flags.png} 
+             name={country.name.official} 
+             region={country.region}
+             capital={country.capital}  
+             population={country.population} 
+           />
+          </Ancle>
+          ))}
+      </Countries>
+  )
 }
 
 export default React.memo(CountriesContainer)
--- src/Services/apiKey.js
+++
@@ -1,1 +0,0 @@
-export const API_KEY = `https://restcountries.com/v3.1/`
