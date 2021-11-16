cea79319d21dede7020e3814a940c3467bd27c34
Dan <danielsalazar.cc@gmail.com>
2021-11-16 00:33:44 -0400

    fix: Update filter

--- src/Services/Filter.js
+++ src/Services/Filter.js
@@ -1,24 +1,20 @@
 export default function countriesFilter(arr, name, region) {
-
-    const countryFilter = arr.filter(item => {
-
-        if (item.name.toLowerCase().startsWith(name.toLowerCase().trim())) {
-            return item;
-        }
-
-        return item.name.toLowerCase() === name.toLowerCase().trim();
-    })
-
-    let resultFilter = countryFilter;
-
-    if (region.length > 1) {
-
-        if (region === "All") resultFilter = countryFilter;
-
-        else resultFilter = countryFilter.filter(item => {
-            return item.region.toLowerCase() === region.toLowerCase().trim();
-        });
+  const countryFilter = arr.filter(item => {
+    if (item.name.official.toLowerCase().includes(name.toLowerCase().trim())) {
+      return item;
     }
 
-    return resultFilter;
+    return item.name.official.toLowerCase() === name.toLowerCase().trim();
+  })
+
+  let resultFilter = countryFilter;
+
+  if (region.length > 1) {
+    if (region === "All") resultFilter = countryFilter;
+    else resultFilter = countryFilter.filter(item => {
+      return item.region.toLowerCase() === region.toLowerCase().trim();
+    });
+  }
+  
+  return resultFilter;
 }
